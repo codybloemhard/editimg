0460b97e536b733cabff826cce956d9d1017cc76
Cody Bloemhard <cblmhrd@live.nl>
2022-09-17 02:27:09 +0200

    added: resizing behaviour and Timer;

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
 use image::io::Reader as IR;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
-use sdl2::rect::Rect;
+use sdl2::{
+    event::{ Event, WindowEvent },
+    keyboard::Keycode,
+    pixels::PixelFormatEnum,
+    rect::Rect,
+    mouse::MouseButton,
+};
 
 use std::time::Instant;
 
 pub fn main() -> Result<(), String> {
-    let start = Instant::now();
-    let ta = start.elapsed().as_millis();
+    // let img = IR::open("/home/cody/img/collections/cltracer/q-bright-sky-rough-copper.png")
+    //     .expect("dsnt").decode().expect("hoed");
+    // let img = IR::open("/home/cody/img/collections/cltracer/t-microfacets-dielectrics-conductors.png")
+    //     .expect("dsnt").decode().expect("hoed");
+
+    let mut timer = Timer::new();
 
     let img = IR::open("/home/cody/img/collections/janitor-pics/14_cracked_stones.png")
-        .expect("dsnt").decode().expect("hoed");
-    let pxs = img.as_rgba8().unwrap();
-    //let pxs = img.to_rgba8();
-    // let pxs = IR::open("/home/cody/img/collections/cltracer/q-bright-sky-rough-copper.png")
-    //     .expect("dsnt").decode().expect("hoed");
-    // let pxs = IR::open("/home/cody/img/collections/cltracer/t-microfacets-dielectrics-conductors.png")
-    //     .expect("dsnt").decode().expect("hoed");
+        .map_err(|e| e.to_string())?
+        .decode()
+        .map_err(|e| e.to_string())?
+        .into_rgba8();
 
-    println!("image: {:?}ms", start.elapsed().as_millis() - ta);
+    let imgw = img.width();
+    let imgh = img.height();
+
+    println!("Image: {:?}ms", timer.elapsed());
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 500, 500)
+        .window("editimg", 512, 512)
         .resizable()
         .opengl()
         .build()
@@ -34,20 +41,15 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    println!("window: {:?}ms", start.elapsed().as_millis() - ta);
+    println!("Window: {:?}ms", timer.elapsed());
 
     let texture_creator = canvas.texture_creator();
     let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGBA32, img.width(), img.height())
+        .create_texture_streaming(PixelFormatEnum::RGBA32, imgw, imgh)
         .map_err(|e| e.to_string())?;
-    texture.update(None, pxs, 4 * img.width() as usize).unwrap();
+    texture.update(None, &img, 4 * imgw as usize).map_err(|e| e.to_string())?;
 
-    println!("texture: {:?}ms", start.elapsed().as_millis() - ta);
-
-    canvas.copy(&texture, None, Some(Rect::new(0, 0, 512, 256)))?;
-    canvas.present();
-
-    println!("present: {:?}ms", start.elapsed().as_millis() - ta);
+    println!("Texture: {:?}ms", timer.elapsed());
 
     let mut event_pump = sdl_context.event_pump()?;
 
@@ -59,15 +61,83 @@ pub fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::Window{ win_event: WindowEvent::Resized(winw, winh), .. } => {
+                    println!("Resized: ({}, {})", winw, winh);
+                    timer.checkpoint();
+                    canvas.clear();
+                    let winw = winw.max(0).unsigned_abs();
+                    let winh = winh.max(0).unsigned_abs();
+                    let (x, y, w, h) = resize_dims(imgw, imgh, winw, winh);
+                    canvas.copy(&texture, None, Some(Rect::new(x, y, w, h)))?;
+                    canvas.present();
+                    println!("Resizing: {:?}ms", timer.elapsed());
+                },
                 Event::KeyDown { .. } => {
                     println!("yeet");
-                    canvas.copy(&texture, None, Some(Rect::new(100, 100, 256, 256)))?;
+                },
+                Event::MouseButtonDown{ mouse_btn: MouseButton::Left, clicks: 1, x, y, .. } => {
+                    println!("Click: ({}, {})", x, y);
                 },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
     }
 
     Ok(())
+}
+
+struct Timer{
+    time: Instant,
+    prev: u128,
+}
+
+impl Timer{
+    fn new() -> Self{
+        let time = Instant::now();
+        let prev = time.elapsed().as_millis();
+        Self{ time, prev }
+    }
+
+    fn elapsed(&self) -> u128{
+        self.time.elapsed().as_millis() - self.prev
+    }
+
+    fn checkpoint(&mut self){
+        self.prev = self.time.elapsed().as_millis();
+    }
+}
+
+fn resize_dims(imgw: u32, imgh: u32, winw: u32, winh: u32) -> (i32, i32, u32, u32){
+    let wfac = winw as f32 / imgw as f32;
+    let hfac = winh as f32 / imgh as f32;
+    let fac = wfac.min(hfac);
+    let w = (imgw as f32 * fac) as u32;
+    let h = (imgh as f32 * fac) as u32;
+    let x = if w < winw - 2 { (winw - w) / 2 } else { 0 } as i32;
+    let y = if h < winh - 2 { (winh - h) / 2 } else { 0 } as i32;
+    (x, y, w, h)
+}
+
+#[cfg(test)]
+mod tests{
+
+    use super::*;
+
+    #[test]
+    fn test_resize_dims(){
+        let (x, y, w, h) = resize_dims(100, 100, 100, 100);
+        assert_eq!((x, y, w, h), (0, 0, 100, 100));
+
+        let (x, y, w, h) = resize_dims(50, 50, 100, 100);
+        assert_eq!((x, y, w, h), (0, 0, 100, 100));
+
+        let (x, y, w, h) = resize_dims(200, 200, 100, 100);
+        assert_eq!((x, y, w, h), (0, 0, 100, 100));
+
+        let (x, y, w, h) = resize_dims(100, 50, 100, 100);
+        assert_eq!((x, y, w, h), (0, 25, 100, 50));
+
+        let (x, y, w, h) = resize_dims(50, 100, 100, 100);
+        assert_eq!((x, y, w, h), (25, 0, 50, 100));
+    }
 }
