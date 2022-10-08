8c4047d3c50020f18f8b5c54877c96fd6936ac7d
Cody Bloemhard <codybloemhard@gmail.com>
2022-10-08 02:08:34 +0200

    refactored: rhai engine;

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod window;
 mod timer;
+mod scripting;
 
 use crate::{
     window::EIWindow,
     timer::Timer,
+    scripting::*,
 };
-
-use rhai::Engine;
 
 use sdl2::{
     event::{ Event, WindowEvent },
@@ -14,55 +14,25 @@ use sdl2::{
     mouse::MouseButton,
 };
 
-use std::sync::mpsc;
-
-#[derive(Debug, Clone)]
-struct RHAIMouseClick{
-    x: i64,
-    y: i64,
-}
-
-impl RHAIMouseClick{
-    fn get_x(&mut self) -> i64 { self.x }
-    fn get_y(&mut self) -> i64 { self.y }
-}
-
 pub fn main() -> Result<(), String> {
-    let (to_host, from_rhai) = mpsc::channel();
-    let (to_rhai, from_host) = mpsc::channel();
+    let (
+        host_portals,
+        RhaiPortals{ mc_to_rhai, from_rhai },
+    ) = create_channels();
 
-    let _rhai_thread_handle = std::thread::spawn(move || {
-        let mut engine = Engine::new();
-
-        engine.register_type_with_name::<RHAIMouseClick>("MouseClick")
-            .register_get("x", RHAIMouseClick::get_x)
-            .register_get("y", RHAIMouseClick::get_y);
-
-        let receive_err = "Editimg: rhai thread could not receive from host.";
-        let send_err = "Editimg: rhai thread could not send to host.";
-        let th0 = to_host.clone();
-        let th1 = to_host.clone();
-
-        // Notice that the API functions are blocking
-        engine
-            .register_fn("get", move || -> RHAIMouseClick {
-                th0.send(-1).expect(send_err);
-                from_host.recv().expect(receive_err)
-            })
-            .register_fn("put", move |v: i64| {
-                th1.send(v).expect(send_err);
-            })
-            ;
+    std::thread::spawn(move || {
+        let engine = construct_rhai_engine(host_portals);
 
         engine.run(
             r#"
                 print("Starting script loop...");
                 for _i in 0..2{
-                    let mc = get();
+                    let mc = get_mouse_click();
                     print(`  Script Read: ${mc.x}, ${mc.y}`);
                     put(mc.x);
                     put(mc.y);
                 }
+                kill();
             "#,
         ).expect("Editimg: rhai error");
     });
@@ -78,8 +48,12 @@ pub fn main() -> Result<(), String> {
 
     'running: loop {
         for rhai_call in from_rhai.try_iter(){
-            println!("Call: {rhai_call}");
-            if rhai_call == -1 { send_next_click = true; }
+            use HostMsg::*;
+            match rhai_call{
+                Kill => break 'running,
+                GetMouseClick => send_next_click = true,
+                msg => println!("{:?}", msg),
+            }
         }
         for event in event_pump.poll_iter() {
             match event {
@@ -102,7 +76,7 @@ pub fn main() -> Result<(), String> {
                 Event::MouseButtonDown{ mouse_btn: MouseButton::Left, clicks: 1, x, y, .. } => {
                     if send_next_click{
                         send_next_click = false;
-                        to_rhai.send(RHAIMouseClick{ x: x as i64, y: y as i64 })
+                        mc_to_rhai.send(MouseClick{ x: x as i64, y: y as i64 })
                             .map_err(|e| e.to_string())?;
                     }
                 },
@@ -111,6 +85,7 @@ pub fn main() -> Result<(), String> {
         }
     }
 
+    println!("Editimg: finished.");
     Ok(())
 }
 
---
+++ src/scripting.rs
@@ -0,0 +1,75 @@
+use rhai::Engine;
+
+use std::sync::mpsc;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HostMsg{
+    Kill,
+    GetMouseClick,
+    DebugI64(i64),
+}
+
+#[derive(Debug, Clone)]
+pub struct MouseClick{
+    pub x: i64,
+    pub y: i64,
+}
+
+impl MouseClick{
+    fn get_x(&mut self) -> i64 { self.x }
+    fn get_y(&mut self) -> i64 { self.y }
+}
+
+pub struct HostPortals{
+    pub to_host: mpsc::Sender<HostMsg>,
+    pub mc_from_host: mpsc::Receiver<MouseClick>,
+}
+
+pub struct RhaiPortals{
+    pub mc_to_rhai: mpsc::Sender<MouseClick>,
+    pub from_rhai: mpsc::Receiver<HostMsg>,
+}
+
+pub fn create_channels() -> (HostPortals, RhaiPortals){
+    let (to_host, from_rhai) = mpsc::channel();
+    let (mc_to_rhai, mc_from_host) = mpsc::channel();
+    (
+        HostPortals{
+            to_host, mc_from_host,
+        },
+        RhaiPortals{
+            mc_to_rhai,
+            from_rhai,
+        }
+    )
+}
+
+pub fn construct_rhai_engine(host_portals: HostPortals) -> Engine {
+    let mut engine = Engine::new();
+
+    let HostPortals{ to_host, mc_from_host } = host_portals;
+
+    engine.register_type_with_name::<MouseClick>("MouseClick")
+        .register_get("x", MouseClick::get_x)
+        .register_get("y", MouseClick::get_y);
+
+    let receive_err = "Editimg: rhai thread could not receive from host.";
+    let send_err = "Editimg: rhai thread could not send to host.";
+    let th0 = to_host.clone();
+    let th1 = to_host.clone();
+
+    engine
+        .register_fn("kill", move || {
+            to_host.send(HostMsg::Kill).expect(send_err);
+        })
+        .register_fn("put", move |v: i64| {
+            th0.send(HostMsg::DebugI64(v)).expect(send_err);
+        })
+        .register_fn("get_mouse_click", move || -> MouseClick {
+            th1.send(HostMsg::GetMouseClick).expect(send_err);
+            mc_from_host.recv().expect(receive_err)
+        })
+    ;
+
+    engine
+}
